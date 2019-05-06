03001fc739888f306600568288a35ae42e7c649b
vikranth_prasad <pulipati.pulipati.prasad4@gmail.com>
2019-05-06 06:15:18 +0530

    adding authorizer

--- src/main/java/com/serverless/ApiAuthenticationHandler.java
+++ src/main/java/com/serverless/ApiAuthenticationHandler.java
@@ -9,9 +9,9 @@ import org.apache.logging.log4j.Logger;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+public class ApiAuthenticationHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
 
-	private static final Logger LOG = LogManager.getLogger(Handler.class);
+	private static final Logger LOG = LogManager.getLogger(ApiAuthenticationHandler.class);
 
 	@Override
 	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
